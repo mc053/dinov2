3ded4e34eb54a7264c5d718f22ec7b24d73ba04c
mc053 <marvin.chen@web.de>
2025-01-19 09:26:06 +0100

    More Evaluation

--- dinov2/data/datasets/rvl_cdip.py
+++ dinov2/data/datasets/rvl_cdip.py
@@ -82,3 +82,9 @@ class RvlCdip100PixelatedTrain(RvlCdipOriginalTrain):
     transform=None, target_transform=None, image_dir_name="RVL-CDIP_100_pixelated/train"):
         super().__init__(root=root, transforms=transforms, transform=transform,
         target_transform=target_transform, image_dir_name=image_dir_name)
+
+class RvlCdip100PixelatedVal(RvlCdipOriginalTrain):
+    def __init__(self, root: str = os.path.dirname(os.path.abspath(__file__)), transforms=None,
+    transform=None, target_transform=None, image_dir_name="RVL-CDIP_100_pixelated/val"):
+        super().__init__(root=root, transforms=transforms, transform=transform,
+        target_transform=target_transform, image_dir_name=image_dir_name)
--- dinov2/data/loaders.py
+++ dinov2/data/loaders.py
@@ -10,7 +10,7 @@ from typing import Any, Callable, List, Optional, TypeVar
 import torch
 from torch.utils.data import Sampler
 
-from .datasets import ImageNet, ImageNet22k, CelebAOriginalTrain, CelebAOriginalVal, CelebAPixelatedTrain, CelebAPixelatedVal, CelebAMaskedTrain, CelebAMaskedVal, RvlCdipOriginalTrain, RvlCdipOriginalVal, RvlCdip100MaskedTrain, RvlCdip100MaskedVal, RvlCdip100PixelatedTrain, CelebAPixelatedABTrain, CelebAMaskedABTrain
+from .datasets import ImageNet, ImageNet22k, CelebAOriginalTrain, CelebAOriginalVal, CelebAPixelatedTrain, CelebAPixelatedVal, CelebAMaskedTrain, CelebAMaskedVal, RvlCdipOriginalTrain, RvlCdipOriginalVal, RvlCdip100MaskedTrain, RvlCdip100MaskedVal, RvlCdip100PixelatedTrain, CelebAPixelatedABTrain, CelebAMaskedABTrain, RvlCdip100PixelatedVal
 from .samplers import EpochSampler, InfiniteSampler, ShardedInfiniteSampler
 
 
@@ -82,6 +82,8 @@ def _parse_dataset_str(dataset_str: str):
         class_ = RvlCdip100MaskedVal
     elif name == "RvlCdip100PixelatedTrain":
         class_ = RvlCdip100PixelatedTrain
+    elif name == "RvlCdip100PixelatedVal":
+        class_ = RvlCdip100PixelatedVal
     elif name == "CelebAMaskedABTrain":
         class_ = CelebAMaskedABTrain
     else:
