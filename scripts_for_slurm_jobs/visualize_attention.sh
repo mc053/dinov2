15a517133d51f4d0ef11460d9c91c1ea044205fa
mc053 <marvin.chen@web.de>
2025-01-23 12:08:16 +0100

    Automate Visualization

--- scripts_for_slurm_jobs/visualize_attention.sh
+++ scripts_for_slurm_jobs/visualize_attention.sh
@@ -13,9 +13,9 @@ echo "Available GPUs: $(nvidia-smi)"
 
 # Start jupyter lab
 srun --unbuffered enroot start --mount $HOME:$HOME/mounted_home -w mc085 bash -c "
-    source /home/stud/m/mc085/mounted_home/dinov2_env/bin/activate &&
+    source /home/stud/m/mc085/mounted_home/pia11_clean/bin/activate &&
     cd /home/stud/m/mc085/mounted_home/dinov2 &&
-    PYTHONPATH=. python visualize_attention.py
+    PYTHONPATH=. python visualize_attention.py --model RVL_CDIP_gt
 "
 
 # when starting with run/train/train.py --nodes 1 --gpus 1
--- visualize_attention.py
+++ visualize_attention.py
@@ -12,19 +12,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Inspired from https://gitlab.com/ziegleto-machine-learning/dino/-/blob/main/visualize_attention.py
+# Taken from https://gitlab.com/ziegleto-machine-learning/dino/-/blob/main/visualize_attention.py
 
 import os
 import sys
 import argparse
-# import cv2
 import random
 import colorsys
 import requests
 from io import BytesIO
-
-# import skimage.io
-# from skimage.measure import find_contours
 import matplotlib.pyplot as plt
 from matplotlib.patches import Polygon
 import torch
@@ -40,40 +36,32 @@ from dinov2.utils.utils import load_pretrained_weights
 from dinov2.models import build_model_from_cfg
 
 if __name__ == '__main__':
-    
-    # image_size = (952, 952)
-    # output_dir = '.'
-    # patch_size = 14
-    image_size = (960, 960)
-    output_dir = '.'
-    patch_size = 16
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model")
+    parser.add_argument("--image")
 
-    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    args = parser.parse_args()
     
-    model_name = "CelebA_gt"
+    model_name = args.model
+    image_name = args.image
+
+    patch_size = 16 # all trained models are ViTs (large) with 16x16 patch sizes.
+    image_size = (1600, 1600) # HxW must be a multiple of 16.
+    output_dir = f'./{model_name}/eval/training_124999/attention_visualization/{image_name}'
     args = Args(config_file=f"./{model_name}/config.yaml")
     cfg = get_cfg_from_args(args)
     model, _ = build_model_from_cfg(cfg, only_teacher=True)
     pretrained_weights = f'./{model_name}/eval/training_124999/teacher_checkpoint.pth'
     load_pretrained_weights(model, pretrained_weights, "teacher")
 
-    # model = vit_large(
-    #         patch_size=14,
-    #         img_size=526,
-    #         init_values=1.0,
-    #         #ffn_layer="mlp",
-    #         block_chunks=0
-    # )
-# 
-    # model.load_state_dict(torch.load('dinov2_vitl14_pretrain.pth')) # torch.load(f'./{model_name}/eval/training_124999/teacher_checkpoint.pth') (?)
-
     for p in model.parameters():
         p.requires_grad = False
 
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu") 
     model.to(device)
     model.eval()
 
-    img = Image.open('./dinov2/data/datasets/CelebA/CelebA_original/val/162772.jpg')
+    img = Image.open(f'./{image_name}')
     img = img.convert('RGB')
     transform = pth_transforms.Compose([
         pth_transforms.Resize(image_size),
@@ -81,38 +69,22 @@ if __name__ == '__main__':
         pth_transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
     ])
     img = transform(img)
-    print(img.shape)
-# 
-    # make the image divisible by the patch size
+
     w, h = img.shape[1] - img.shape[1] % patch_size, img.shape[2] - img.shape[2] % patch_size
     img = img[:, :w, :h].unsqueeze(0)
-# 
+ 
     w_featmap = img.shape[-2] // patch_size
     h_featmap = img.shape[-1] // patch_size
-# 
-    print(img.shape)
-# 
+
     attentions = model.get_last_self_attention(img.to(device))
-# 
     nh = attentions.shape[1] # number of head
-    # print(f"Shape of attentions: {attentions.shape}")
-    # print(nh)
-# 
-    # we keep only the output patch attention
-    # for every patch
     attentions = attentions[0, :, 0, 1:].reshape(nh, -1)
-    # # weird: one pixel gets high attention over all heads?
-    # print(torch.max(attentions, dim=1)) 
-    # attentions[:, 283] = 0 
-# # 
     attentions = attentions.reshape(nh, w_featmap, h_featmap)
     attentions = nn.functional.interpolate(attentions.unsqueeze(0), scale_factor=patch_size, mode="nearest")[0].cpu().numpy()
-# ##
-    # save attentions heatmaps
+
     os.makedirs(output_dir, exist_ok=True)
-# ##
+
     for j in range(nh):
         fname = os.path.join(output_dir, "attn-head" + str(j) + ".png")
         plt.imsave(fname=fname, arr=attentions[j], format='png')
         print(f"{fname} saved.")
-# # 
