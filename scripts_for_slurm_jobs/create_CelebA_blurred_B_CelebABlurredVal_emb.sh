d13db6593e2e954d2a52c3810393ddb8117ed51a
mc053 <marvin.chen@web.de>
2025-02-15 11:37:04 +0100

    Starting Blurred Evaluation

---
+++ scripts_for_slurm_jobs/create_CelebA_blurred_B_CelebABlurredVal_emb.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name create-celeba-blurred-b-celebablurredval-emb
+#SBATCH --output create-celeba-blurred-b-celebablurredval-emb-%j.out
+#SBATCH --partition gpu
+#SBATCH --gpus 1
+#SBATCH --nodelist=tars
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=mc085@hdm-stuttgart.de
+
+# Print some node information
+echo "$(date)"
+echo "Start creating CelebA_blurred_B_CelebABlurredVal_emb on partition: GPU"
+echo "Running on: $(hostname)"
+echo "Available CPUs: $(taskset -c -p $$) (logical CPU ids)"
+echo "Available GPUs: $(nvidia-smi)"
+
+# Start jupyter lab
+srun --unbuffered enroot start --mount $HOME:$HOME/mounted_home -w mc085 bash -c "
+    source /home/stud/m/mc085/mounted_home/dinov2_env/bin/activate &&
+    cd /home/stud/m/mc085/mounted_home/dinov2 &&
+    PYTHONPATH=. python embeddings.py --model CelebA_blurred_B --input CelebABlurredVal
+"
---
+++ scripts_for_slurm_jobs/eval_linear_gender_CelebA_gt_with_blurred_dataset.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --job-name eval-linear-gender-celeba-gt-with-blurred-dataset
+#SBATCH --output eval-linear-gender-celeba-gt-with-blurred-dataset-%j.out
+#SBATCH --partition gpu
+#SBATCH --gpus 1
+#SBATCH --nodelist=tars
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=mc085@hdm-stuttgart.de
+
+# Print some node information
+echo "$(date)"
+echo "Starting CelebA_gt linear gender evaluation with blurred train and val dataset on partition: GPU"
+echo "Running on: $(hostname)"
+echo "Available CPUs: $(taskset -c -p $$) (logical CPU ids)"
+echo "Available GPUs: $(nvidia-smi)"
+
+# Start jupyter lab
+srun --unbuffered enroot start --mount $HOME:$HOME/mounted_home -w mc085 bash -c "
+    source /home/stud/m/mc085/mounted_home/dinov2_env/bin/activate &&
+    cd /home/stud/m/mc085/mounted_home/dinov2 &&
+    PYTHONPATH=. python dinov2/eval/linear.py \
+    --config-file CelebA_gt/config.yaml \
+    --pretrained-weights CelebA_gt/eval/training_124999/teacher_checkpoint.pth \
+    --output-dir CelebA_gt/eval/training_124999/linear_gender_with_blurred_dataset \
+    --train-dataset CelebABlurredTrain \
+    --val-dataset CelebABlurredVal
+"
