654f2b730b5c122ce941e0765397a81683636a6f
mc053 <marvin.chen@web.de>
2025-02-18 18:14:37 +0100

    More Evaluation

---
+++ scripts_for_slurm_jobs/eval_retrieval_mndcg_5%_CelebA_blurred_C_CelebAOriginalVal_emb->CelebA_blurred_C_CelebABlurredVal_emb.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --job-name eval-retrieval-mndcg-5percent-CelebA_blurred_C_CelebAOriginalVal_emb->CelebA_blurred_C_CelebABlurredVal_emb
+#SBATCH --output eval-retrieval-mndcg-5percent-CelebA_blurred_C_CelebAOriginalVal_emb->CelebA_blurred_C_CelebABlurredVal_emb-%j.out
+#SBATCH --partition gpu
+#SBATCH --gpus 1
+#SBATCH --nodelist=tars
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=mc085@hdm-stuttgart.de
+
+# Print some node information
+echo "$(date)"
+echo "Starting CelebA retrieval mnDCG evaluation for scenario "Adaption C/Blur with Unanonymized Query Image" on partition: GPU"
+echo "Running on: $(hostname)"
+echo "Available CPUs: $(taskset -c -p $$) (logical CPU ids)"
+echo "Available GPUs: $(nvidia-smi)"
+
+# Start jupyter lab
+srun --unbuffered enroot start --mount $HOME:$HOME/mounted_home -w mc085 bash -c "
+    source /home/stud/m/mc085/mounted_home/pia11_clean/bin/activate &&
+    cd /home/stud/m/mc085/mounted_home/dinov2 &&
+    PYTHONPATH=. python mean_normalized_discounted_cumulative_gain.py \
+    --gt CelebA_retrieval_ground_truths.pkl \
+    --percent 5 \
+    --query CelebA_blurred_C_CelebAOriginalVal_emb.json \
+    --database CelebA_blurred_C_CelebABlurredVal_emb.json
+"
