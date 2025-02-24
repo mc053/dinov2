0d59c9c06fa42d41a4f7802a0a948b85dae5b6a5
mc053 <marvin.chen@web.de>
2025-02-24 17:43:02 +0100

    More Evaluation

---
+++ scripts_for_slurm_jobs/eval_linear_class_RVL_CDIP_gt_with_100_blurred_dataset.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --job-name eval-linear-class-rvl-cdip-gt-with-100-blurred-dataset
+#SBATCH --output eval-linear-class-rvl-cdip-gt-with-100-blurred-dataset-%j.out
+#SBATCH --partition gpu
+#SBATCH --gpus 1
+#SBATCH --nodelist=tars
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=mc085@hdm-stuttgart.de
+
+# Print some node information
+echo "$(date)"
+echo "Starting RVL_CDIP_gt linear class evaluation with 100% blurred train and val dataset on partition: GPU"
+echo "Running on: $(hostname)"
+echo "Available CPUs: $(taskset -c -p $$) (logical CPU ids)"
+echo "Available GPUs: $(nvidia-smi)"
+
+# Start jupyter lab
+srun --unbuffered enroot start --mount $HOME:$HOME/mounted_home -w mc085 bash -c "
+    source /home/stud/m/mc085/mounted_home/dinov2_env/bin/activate &&
+    cd /home/stud/m/mc085/mounted_home/dinov2 &&
+    PYTHONPATH=. python dinov2/eval/linear.py \
+    --config-file RVL_CDIP_gt/config.yaml \
+    --pretrained-weights RVL_CDIP_gt/eval/training_124999/teacher_checkpoint.pth \
+    --output-dir RVL_CDIP_gt/eval/training_124999/linear_class_with_100_blurred_dataset \
+    --train-dataset RvlCdip100BlurredTrain \
+    --val-dataset RvlCdip100BlurredVal
+"
--- scripts_for_slurm_jobs/eval_retrieval_mndcg_5%_RVL_CDIP_100_blurred_C_RvlCdip100BlurredVal_emb->RVL_CDIP_100_blurred_C_RvlCdip100BlurredVal_emb.sh
+++ scripts_for_slurm_jobs/eval_retrieval_mndcg_5%_RVL_CDIP_100_blurred_C_RvlCdip100BlurredVal_emb->RVL_CDIP_100_blurred_C_RvlCdip100BlurredVal_emb.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-#SBATCH --job-name eval-retrieval-map-5percent-RVL_CDIP_100_blurred_C_RvlCdip100BlurredVal_emb->RVL_CDIP_100_blurred_C_RvlCdip100BlurredVal_emb
-#SBATCH --output eval-retrieval-map-5percent-RVL_CDIP_100_blurred_C_RvlCdip100BlurredVal_emb->RVL_CDIP_100_blurred_C_RvlCdip100BlurredVal_emb-%j.out
-#SBATCH --cpus-per-task 4
+#SBATCH --job-name eval-retrieval-mndcg-5percent-RVL_CDIP_100_blurred_C_RvlCdip100BlurredVal_emb->RVL_CDIP_100_blurred_C_RvlCdip100BlurredVal_emb
+#SBATCH --output eval-retrieval-mndcg-5percent-RVL_CDIP_100_blurred_C_RvlCdip100BlurredVal_emb->RVL_CDIP_100_blurred_C_RvlCdip100BlurredVal_emb-%j.out
+#SBATCH --partition gpu
+#SBATCH --gpus 1
+#SBATCH --nodelist=ada
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=mc085@hdm-stuttgart.de
 
 # Print some node information
 echo "$(date)"
-echo "Starting RVL CDIP retrieval mAP evaluation for scenario "Adaption C/Blurred" (100%) with Anonymized Query Image on partition: CPU"
+echo "Starting RVL CDIP retrieval mnDCG evaluation for scenario "Adaption C/Blurred" (100%) with Anonymized Query Image on partition: CPU"
 echo "Running on: $(hostname)"
 echo "Available CPUs: $(taskset -c -p $$) (logical CPU ids)"
 echo "Available GPUs: $(nvidia-smi)"
@@ -16,7 +18,7 @@ echo "Available GPUs: $(nvidia-smi)"
 srun --unbuffered enroot start --mount $HOME:$HOME/mounted_home -w mc085 bash -c "
     source /home/stud/m/mc085/mounted_home/pia11_clean/bin/activate &&
     cd /home/stud/m/mc085/mounted_home/dinov2 &&
-    PYTHONPATH=. python mean_average_precision.py \
+    PYTHONPATH=. python mean_normalized_discounted_cumulative_gain.py \
     --gt RVL_CDIP \
     --percent 5 \
     --query RVL_CDIP_100_blurred_C_RvlCdip100BlurredVal_emb.json \
